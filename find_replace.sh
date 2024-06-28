0ab6ac06575db7039e1562d7e4c0ee112b91d6c2
Krishna <sonawanekrishna9130@gmail.com>
2024-06-28 21:51:11 +0530

    "1st find file if not exist or word is not present in the file then it will exit from shell script"

--- find_replace.sh
+++ find_replace.sh
@@ -1,16 +1,41 @@
 #!/bin/bash
-
-read -p 'Enter word want to search: ' Name
-
 read -p 'Enter file name: ' file_name
+echo "$file_name"
+ 
+if [ -f $file_name ]
+then
+	echo "File is present in the directory"
+else
+	echo "File is not present inside the directory"
+	exit
+fi
+echo
+
+#Which string want to search
+read -p 'Enter word want to search: ' String_Name
+echo "$String_Name"
+
+if grep -q "$String_Name" "$file_name"
+then
+	echo "$String_Name is found in $file_name"
+else
+	echo "$String_Name is not found in $file_name"
+	exit
+fi
+echo
+
+
+#read -p 'Enter the Column number; ' n
 
 Awk_use(){
 
-	awk /$Name/'{print}' $file_name
+	awk /$String_Name/'{print}' $file_name
 
 }
 
 Awk_use
+
+echo
 
 read -p "which word want to change: " Name1
 read -p "Replacing Name: " Replace_name
