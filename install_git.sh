caba167758e3160bac6601420576b0d463bae6fd
Krishna <sonawanekrishna9130@gmail.com>
2024-07-18 07:29:07 +0530

    Check the git is installed or not if not then it will install it.

--- install_git.sh
+++ install_git.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
+#Check git is install or not if not then install it.
 
-sudo apt update
-echo "######################################################"
-echo
-sudo apt-get install git -y
-echo "######################################################"
-echo
+check_git_installed() {
+    if command -v git &>/dev/null; then
+        echo "git is already installed. Skipping installation."
+        return 0
+else
+        echo "git is not installed"
+        sudo apt update                              #update the system
+	apt-get install git -y                       #install git using apt
+	return 1
+    fi
+}
+
+check_git_installed
+
+echo "**Git Version**"
 git --version
 echo "######################################################"
 echo
