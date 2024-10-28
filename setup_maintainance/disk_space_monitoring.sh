c64e0db286ce91ffdcae26989766ba949c1f0893
Ubuntu <ubuntu@ip-172-31-0-235.ap-south-1.compute.internal>
2024-10-28 05:56:01 +0000

    For check the disk space of root directory or any directory

---
+++ setup_maintainance/disk_space_monitoring.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Threshold in percentage
+THRESHOLD=23
+
+# Check disk usage for root (/)
+USAGE=$(df -h / | grep / | awk '{ print $5 }' | sed 's/%//g')
+
+# check disk space is not more than Threshold
+if [ "$USAGE" -gt "$THRESHOLD" ]; then
+	echo "Disk space usage is exceeded more than defined Threashold is $THRESHOLD & Current Usage is at ${USAGE}% on / (root) directory"
+
+else
+  echo "Disk space usage is under control."
+fi
+
